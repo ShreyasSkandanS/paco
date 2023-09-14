fdee38402d71b17cdca6daf6086f47010a9ec78b
ShreyasSkandanS <sshreyas@seas.upenn.edu>
2023-09-14 15:06:01 -0400

    bbox stat check

--- scripts/visualize_dataset.py
+++ scripts/visualize_dataset.py
@@ -1,5 +1,8 @@
 import json
 import cv2
+import random
+import numpy as np
+import matplotlib.pyplot as plt
 
 train_json_path = 'paco_cat_v1_train.json'
 test_json_path = 'paco_cat_v1_test.json'
@@ -18,13 +21,57 @@ with open(cat_id_to_name) as f:
 print("Total training samples: {}".format(len(train_dataset)))
 print("Total test samples: {}".format(len(test_dataset)))
 
-train_indices = [34, 200, 455]
-test_indices = [45, 129, 873]
+train_indices = random.sample(range(0, len(train_dataset)), 5)
+test_indices = random.sample(range(0, len(train_dataset)), 5)
 
-indices = test_indices
-dataset = test_dataset
+# indices = test_indices
+# dataset = test_dataset
 # indices = train_indices
 # dataset = train_dataset
+indices = range(0, len(train_dataset))
+dataset = train_dataset
+
+image_area_list = []
+bbox_area_list = []
+relative_area_list = []
+cat_count = 0
+cat_name = 'drill'
+cat_bbox_list = []
+for idx in indices:
+    sample = train_dataset[idx]
+    fname = sample['file_name']
+    image_area = sample['height'] * sample['width']
+    image_area_list.append(image_area)
+    for anno in sample['annotations']:
+        bbox_xywh = anno['bbox']
+        bbox_area = bbox_xywh[2] * bbox_xywh[3]
+        bbox_area_list.append(bbox_area)
+        relative_area_list.append(bbox_area / image_area)
+        if cat_id_to_name[str(anno['category_id'])] == cat_name:
+            cat_count += 1
+            cat_bbox_list.append(bbox_area)
+
+rel_bbox_areas = np.asarray(relative_area_list)
+print("[Rel] Average bbox size: {}".format(np.mean(rel_bbox_areas)))
+print("[Rel] Minimum bbox size: {}".format(np.min(rel_bbox_areas)))
+print("[Rel] Maximum bbox size: {}".format(np.max(rel_bbox_areas)))
+
+abs_bbox_areas = np.asarray(bbox_area_list)
+print("[Abs] Average bbox size: {}".format(np.mean(abs_bbox_areas)))
+print("[Abs] Minimum bbox size: {}".format(np.min(abs_bbox_areas)))
+print("[Abs] Maximum bbox size: {}".format(np.max(abs_bbox_areas)))
+
+hist, bin_edges = np.histogram(rel_bbox_areas, bins=10, range=(0.0, 1.0))
+plt.bar(bin_edges[:-1], hist, width=0.1, color='blue', align='edge')
+plt.title('Relative Box Area Histogram')
+plt.show()
+
+bbox_small = np.sum(abs_bbox_areas < 32**2)
+print("Total COCO-small: {}".format(bbox_small))
+bbox_med = np.sum(abs_bbox_areas < 96**2) - bbox_small
+print("Total COCO-medium: {}".format(bbox_med))
+bbox_large = np.sum(abs_bbox_areas < 1e5**2) - bbox_med - bbox_small
+print("Total COCO-large: {}".format(bbox_large))
 
 for idx in indices:
     sample = train_dataset[idx]
